1d619b1ab310af16106f7034a89a62799b7db302
Shiksha630 <pashiksha2005@gmail.com>
2025-10-19 18:47:16 +0530

    Implement function to find smallest element on left

---
+++ 15-10-2025/SmallElementOnLeft.cpp
@@ -0,0 +1,19 @@
+vector<int> Smallestonleft(int arr[], int n) {
+    vector<int> ans;
+    set<int> s;
+
+    for (int i = 0; i < n; i++) {
+        auto it = s.lower_bound(arr[i]);  
+        
+        if (it == s.begin()) {
+            ans.push_back(-1);
+        } else {
+            it--;  
+            ans.push_back(*it);
+        }
+
+        s.insert(arr[i]);  
+    }
+
+    return ans;
+}
